4087d6acbb4cd35cc8764e036f08cd4ef3641b2f
oscar diaz espinoza <oscar diaz espinoza>
2016-05-04 12:36:55 -0300

    nuevo stck parte vacia

--- src/main/java/cl/ubb/agil/Pila.java
+++ src/main/java/cl/ubb/agil/Pila.java
@@ -3,9 +3,26 @@ package cl.ubb.agil;
 
 
 public class Pila {
-	
+	 private int[] stck = new int [10];
+	 private int tos = -1;
+	 
 	public Pila(){
 		
+	     
 	}
+	
+	public boolean pilavacia() {
+		if(tos == -1){
+			return true;
+		}else{
+			return false;
+		}
+	}
+		
+	
+	
+	
+	
+	
 
 }
--- src/test/java/cl/ubb/agil/PilaTest.java
+++ src/test/java/cl/ubb/agil/PilaTest.java
@@ -2,11 +2,26 @@ package cl.ubb.agil;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+
 public class PilaTest {
 	
 	@Test
 	public void CreatePila() {
 		Pila pil = new Pila();
+	}
+	
+	@Test
+	public void agregaunostacknovacio(){
+		
+		Pila pil = new Pila();
+		
+		boolean result = pil.pilavacia();
+		
+		assertThat (result,is(true));
+		
 	}
 
 }
